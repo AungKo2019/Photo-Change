f1879cae8eefb13213a216b5e2e656dca879043f
AungKo2019 <highflier.ako1991@gmail.com>
2023-03-22 23:32:02 +0630

    first commit

---
+++ App.js
@@ -0,0 +1,40 @@
+import { StatusBar } from 'expo-status-bar';
+import { StyleSheet, Text, View ,Image} from 'react-native';
+import ImageViewer from './components/ImageViewer';
+import Button from './components/Button';
+
+const PlaceholderImage =require('./assets/icon1.png');
+
+export default function App() {
+  return (
+    <View style={styles.container}>
+      <View style={styles.imageContainer}>
+          <ImageViewer placeholderImageSource={PlaceholderImage} style={styles.image}/>
+      </View>
+      <View style={{flex:1/3,alignItems:'center'}}>
+          <Button theme="primary" label={'Choose a photo'}/>
+          <Button label={'Use this photo'}/>
+      </View>
+      <StatusBar style="auto" />
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#25292e',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+  imageContainer:{
+    flex:1,
+    paddingTop:58,
+    //marginTop:58,
+  },
+  image:{
+    width:320,
+    height:440,
+    borderRadius:18,
+  }
+});
